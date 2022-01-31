17b0db11afd7eae4362e88f0359b83c291ed1f10
Tanisshh <tanjilislam288@gmail.com>
2022-01-31 20:30:38 +0600

    creating group Html

--- social/group/views.py
+++ social/group/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views import generic
+from django.urls import reverse
+from group.models import Group, GroupMember
 
-# Create your views here.
+
+class CreatePost(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroup(generic.ListView):
+    model= Group
